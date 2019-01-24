b17ccb7ce582c3738ab42a9c3350abc4f40c1f69
beardofdoom <marcin.jusiak@gmail.com>
2019-01-25 00:40:05 +0100

    Updates

---
+++ precache-manifest.586f00fcb785f313722c73a3f830a37b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3cf1163772f9f8b86a",
+    "url": "/react-clock/static/js/runtime~main.6e3cf116.js"
+  },
+  {
+    "revision": "db80b8afc9b94c94aed9",
+    "url": "/react-clock/static/js/main.db80b8af.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-clock/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "db80b8afc9b94c94aed9",
+    "url": "/react-clock/static/css/main.913a992d.chunk.css"
+  },
+  {
+    "revision": "fb2393cef4791d01b72a4b7d038462b7",
+    "url": "/react-clock/index.html"
+  }
+];
